5655b677f905c5c82cf6342562ae8e83a06a9317
danial-kazemi <93821704+danial-kazemi@users.noreply.github.com>
2023-08-18 21:31:42 +0330

    create search component

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,10 +1,14 @@
+import { useRef } from 'react';
 import { FaPlus } from 'react-icons/fa';
+
 const AddItem = ({ newItem, setNewItem , handleSubmit }) => {
+  const inputRef = useRef();
   return (
     <form className="addForm" onSubmit={handleSubmit}>
             <label htmlFor="addItem" />
             <input 
                 autoFocus
+                ref={inputRef}
                 id='addItem'
                 type='text'
                 placeholder='Add Item'
@@ -13,8 +17,9 @@ const AddItem = ({ newItem, setNewItem , handleSubmit }) => {
                 required
             />
             <button 
-            type='submit'
-            aria-label="Add Item"
+              type='submit'
+              aria-label="Add Item"
+              onClick={() => inputRef.current.focus()}
             >
                 <FaPlus />
             </button>
--- src/App.js
+++ src/App.js
@@ -3,56 +3,42 @@ import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 import AddItem from './AddItem';
+import Search from './Search';
 
 function App() {
 
-  const [items, setItems] = useState([
-    {
-        id : 1,
-        checked: false,
-        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-    },
-    {
-        id : 2,
-        checked: false,
-        item: "item 2"
-    },
-    {
-        id : 3,
-        checked: false,
-        item: "item 3"
-    },
-    {
-        id : 4,
-        checked: false,
-        item: "item 4"
-    },
-    {
-        id : 5,
-        checked: false,
-        item: "item 5"
-    }
-
-]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
 
 const [newItem,setNewItem] = useState('');
 
+const [search, setSearch] = useState('');
+
+const setAndSaveItems = (newItems) => {
+  setItems(newItems);
+  localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+}
+const addItem = (item) => {
+  const id = items.length ? items[items.length - 1].id + 1 : 1
+  const myNewItem = {id, checked:false, item };
+  const listItems = [...items, myNewItem];
+  setAndSaveItems(listItems);
+
+}
 const handleSubmit = (e) => {
   e.preventDefault();
-  console.log(e)
+   if (!newItem) return;   
+   addItem(newItem);
+   setNewItem('');
 }
 
-const handleCheck = (id)=> {       
-      
-  const listItems = items.map( (item)=> item.id === id ? { ...item , checked: !item.checked , des: "Danial"} : item  ); 
-  setItems(listItems);
-  localStorage.setItem('shoppinglist',JSON.stringify(listItems));
+const handleCheck = (id)=> {      
+  const listItems = items.map( (item)=> item.id === id ? { ...item , checked: !item.checked } : item  ); 
+  setAndSaveItems(listItems);  
 }
 
 const handleDelete = (id) =>{
-   const listItems = items.filter( (item)=> item.id !== id   );       
-   setItems(listItems);
-   localStorage.setItem('shoppinglist',JSON.stringify(listItems));
+   const listItems = items.filter( (item)=> item.id !== id   );    
+   setAndSaveItems(listItems);  
 }
   
   return (
@@ -63,8 +49,14 @@ const handleDelete = (id) =>{
         setNewItem = {setNewItem}
         handleSubmit = {handleSubmit}
       />
+      <Search search={search} setSearch={setSearch} />
+      <AddItem 
+        newItem = {newItem}
+        setNewItem = {setNewItem}
+        handleSubmit = {handleSubmit}
+      />
       <Content
-         items={items}         
+         items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLocaleLowerCase()))}         
          handleCheck={handleCheck}
          handleDelete={handleDelete}
       />
