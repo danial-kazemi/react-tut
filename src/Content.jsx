7bb7b3ad12cb77cf4e190d82b1a9c8d5f7970ec6
danial-kazemi <93821704+danial-kazemi@users.noreply.github.com>
2023-08-18 14:10:17 +0330

    add style and finsh delete item

--- src/Content.jsx
+++ src/Content.jsx
@@ -35,36 +35,44 @@ const Content = () => {
       
        const listItems = items.map( (item)=> item.id === id ? { ...item , checked: !item.checked , des: "Danial"} : item  ); 
        setItems(listItems);
+       localStorage.setItem('shoppinglist',JSON.stringify(listItems));
     }
 
     const deleteHandler = (id) =>{
         const listItems = items.filter( (item)=> item.id !== id   );       
-       setItems(listItems);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist',JSON.stringify(listItems));
     }
 
-  return (
-    <main>
-        <ul>
-            {items.map((item) =>
-                (
-                    <li id={item.id} className='item' key={item.id}>
-                        <input
-                            onChange={()=> handelCheck(item.id)}
-                             type="checkbox" 
-                             checked={item.checked} 
-                        />
-                        <label htmlFor="item.id">{item.item}</label>
-                        <FaTrashAlt onClick={() => deleteHandler(item.id)}
-                            role='button' 
-                            tabIndex={0} 
-                         />
-                    </li>
-                )
-            )}
-        </ul>
-        
-        
-    </main>
+  return (   
+ 
+        <main>
+            { items.length ? (   
+                <ul>
+                    {items.map((item) =>
+                        (
+                            <li id={item.id} className='item' key={item.id}>
+                                <input
+                                    onChange={()=> handelCheck(item.id)}
+                                        type="checkbox" 
+                                        checked={item.checked} 
+                                />
+                                <label 
+                                style={ (item.checked) ?{ textDecoration : 'line-through'}: null} onDoubleClick={()=> handelCheck(item.id)} htmlFor="item.id">{item.item}</label>
+                                <FaTrashAlt onClick={() => deleteHandler(item.id)}
+                                    role='button' 
+                                    tabIndex={0} 
+                                    />
+                            </li>
+                        )
+                    )}
+                </ul>
+            ) : (
+                <p style={{marginTop: '2rem', color: 'red'}}>The List is Empty</p>
+            )
+                
+            }
+        </main>
     
   )
 }
